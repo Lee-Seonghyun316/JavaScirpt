e349bc9e1751e9b6a9e7e9e6198bce107e9ce8dd
Lee-SeongHyun316 <mn0316@naver.com>
2021-10-14 08:39:25 +0900

    [FIX] todo_list.js

--- 2021/October/10.13/todo_list.js
+++ 2021/October/10.13/todo_list.js
@@ -5,33 +5,34 @@
 // // 피드백 : onAdd 안에서 delete를 만들면.. 조금 더 지정을 정확하게 할 수 있을 듯
 // }
 const list = document.querySelector('.list');
-const newText = document.querySelector('.new_text')
+const newText = document.querySelector('.new_text');
+const addBtn = document.querySelector('.add_btn');
 
 const onAdd = () => {
     const item = document.createElement('li');
-    item.className = 'item';
     const text = document.createElement('span');
-    text.className = 'text';
-    text.textContent = newText.value;
-    item.appendChild(text);
-    newText.value = ''
-    newText.focus();
     const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete_btn';
-    deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
     const line = document.createElement('div');
-    line.className = 'item_line';
     const onDelete = () => {
         list.removeChild(item);
         list.removeChild(line)
         newText.focus();
     }
+    item.className = 'item';
+    text.className = 'text';
+    line.className = 'item_line';
+    deleteButton.className = 'delete_btn';
+    text.textContent = newText.value;
+    deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
     deleteButton.addEventListener('click', onDelete);
+    item.appendChild(text);
     item.appendChild(deleteButton);
     list.appendChild(item);
     list.appendChild(line);
+    newText.value = ''
+    newText.focus();
 }
-const addBtn = document.querySelector('.add_btn');
+
 addBtn.addEventListener('click', onAdd);
 newText.addEventListener('keypress', (event) => {
     console.log(event);
@@ -39,5 +40,5 @@ newText.addEventListener('keypress', (event) => {
         onAdd();
     }
 });
-//피드백 : input에 이벤트 추가해야 정상 작동
+//피드백 : input 에 keypress 이벤트 추가해야 정상 작동
 
