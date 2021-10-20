e7149d4a05d752805f42857540be407825bbdffd
Lee-SeongHyun316 <mn0316@naver.com>
2021-10-20 13:27:07 +0900

    [Carrot Game] 피드백 추가

--- game_carrot/index.js
+++ game_carrot/index.js
@@ -8,22 +8,27 @@ const popUp = document.querySelector(`.pop_up`);
 const popUpMessage = document.querySelector(`.pop_up_message`);
 const refresh = document.querySelector(`.refresh`);
 const timer = document.querySelector(`.timer`);
+
 const rand = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 const makeItem = (name) => {
+    //피드백 : 0~640, 0~170 범위 정할 때 유추대신 rect 활용해서 범위 정하기 -> 반영
+    //피드백 : translate 대신 position : absolute, top, left 활용 -> 왜?
+    //피드백 : addEventListen, appendChild 까지 할 수 있도록 일반적 함수 형성
+
     let height = rand(0, 640);
     let width = rand(0, 170);
     let item = document.createElement('div');
     item.className = `${name}`;
     item.style.transform = `translate(${height}px, ${width}px)`;
-
     return item;
 }
 const init = () => {
     deleteAllItem();
-    start.className=`game_button game_button_hide`;
-    stop.className=`game_button`;
+    start.className = `game_button game_button_hide`;
+    stop.className = `game_button`;
     for (let i = 0; i < MAX_ITEM; i++) {
         let carrot = makeItem('carrot');
         carrot.addEventListener('click', onCarrot);
@@ -32,15 +37,22 @@ const init = () => {
         gameField.appendChild(carrot);
         gameField.appendChild(bug);
     }
+    onTimer();
 }
 
 const onTimer = () => {
+    //피드백 : clearTimeout 사용해서 setInterval 끝내기
+    // 정지 버튼 둘렀을 때 타이머 정지 -> 재생 시 다시시작
+    // 분(minutes)도 계산해서 나타낼 수 있도록 함수 구현
     let time = 10;
-    window.setInterval(()=> {
+    let timeRepeat = setInterval(() => {
         time -= 1;
-        timer.textContent = time;
-        if(timer.textContent === '0'){
-            onBug();
+        timer.textContent = `0:${time}`;
+        if(time <= 0){
+            clearTimeout(timeRepeat);
+            popUp.className = 'pop_up';
+            popUpMessage.textContent = `TIME OVER`;
+            return;
         }
     }, 1000);
 }
@@ -52,7 +64,8 @@ const onStop = () => {
 }
 
 const deleteAllItem = () => {
-    while (gameField.hasChildNodes()) {	// 부모노드가 자식이 있는지 여부를 알아낸다
+    //피드백 : innerHTML = ''로 바꾸기?
+    while (gameField.hasChildNodes()) {
         gameField.removeChild(
             gameField.firstChild
         );
@@ -65,7 +78,7 @@ const onCarrot = (event) => {
     let carrots = document.querySelectorAll(`.carrot`);
     console.log(carrots.length);
     count.textContent = `${carrots.length}`;
-    if(carrots.length === 0){
+    if (carrots.length === 0) {
         popUp.className = 'pop_up';
         console.log(popUp.className);
         popUpMessage.textContent = `YOU WON`;
